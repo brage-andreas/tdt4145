044fd46ec54b13c2faea7b9d40021d61f26bfe1a
brage-andreas <bragerino+github@gmail.com>
2025-03-14 10:02:37 +0100

    feat: add SQL scripts for populating flight and ticket price data, and create query for airline fleet information

---
+++ src/sql/task-5.sql
@@ -0,0 +1,18 @@
+/*
+Vi ønsker å ha en SQL-spørring (query) som finner flyselskap, hvilke flytyper
+flyselskapet bruker og antall fly selskapet har av hver flytype.
+ */
+SELECT
+  Flyselskap.Navn AS Flyselskap,
+  Flytype.Navn AS Flytype,
+  COUNT(Fly.Registreringsnummer) AS AntallFly
+FROM
+  Flyselskap
+  JOIN Fly ON Flyselskap.Flyselskapkode = Fly.FlyselskapKode
+  JOIN Flytype ON Fly.FlytypeNavn = Flytype.Navn
+GROUP BY
+  Flyselskap.Navn,
+  Flytype.Navn
+ORDER BY
+  Flyselskap.Navn,
+  Flytype.Navn;
